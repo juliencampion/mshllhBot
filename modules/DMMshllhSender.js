3dc51bd944601193e50f7bb55b607f7b1e5f49ac
Sylvain Chaugny <sylvain@slaask.com>
2019-05-23 12:41:53 +0200

    fix

--- modules/DMMshllhSender.js
+++ modules/DMMshllhSender.js
@@ -4,7 +4,7 @@ const Logger = require("@elian-wonhalf/pretty-logger")
 
 class DMMshllhSender extends BaseModule {
     canProcess(message) {
-        return Utils.rand() && message.author.id != this.constants.bot_id
+        return Utils.rand() == 42 && message.author.id != this.constants.bot_id
     }
 	process(message) {
         message.author.send({ file: this.constants.mashallah_image }).catch(Logger.exception);
