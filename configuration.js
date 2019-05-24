3ed0f02fe776fcb97fe0ef3ba6a38e2661344ef5
Sylvain Chaugny <sylvain@slaask.com>
2019-05-24 11:50:28 +0200

    [ADD] rt si t trist

--- configuration.js
+++ configuration.js
@@ -2,7 +2,7 @@ const constants = require("./constants.js")
 
 module.exports = {
 	events: ["message", "messageDelete", "messageUpdate"],
-	modules: ["Censorship", "MessageUpdateTracker", "DMMshllhSender", "GayCommand", "MashallahImageSender", "MessageUpdateTracker", "NoHomoMessageSender", "PutTheBell", "RandomInteractions", "9GagReplacer", "AsciiReactions"],
+	modules: ["Censorship", "MessageUpdateTracker", "DMMshllhSender", "GayCommand", "MashallahImageSender", "MessageUpdateTracker", "NoHomoMessageSender", "PutTheBell", "RandomInteractions", "9GagReplacer", "AsciiReactions", "RTSiTTrist"],
 	events_configuration: {
 		'message': function(message) {
 			return !message.author.bot && message.author.id != constants.bot_id
