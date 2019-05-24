51cdc00ce704d689fa6c2d965404442880e7b579
Simon Desplat <simon.desplat@itelios.com>
2019-05-24 16:35:09 +0200

    fixing best feature

--- modules/MashallahImageSender.js
+++ modules/MashallahImageSender.js
@@ -3,15 +3,11 @@ const Utils = require("../Utils.js")
 const Logger = require("@elian-wonhalf/pretty-logger")
 
 class MashallahImageSender extends BaseModule {
-	canProcess(message) {
-		return message.content.startsWith(this.constants.command_prefix)
-	}
-
 	process(message) {
 		var sentence = message.content.split(' ')
 		const mshllh = ['mshllh', 'mashallah']
 
-		for (word = 0; word < sentence.length; word++) {
+		for (var word = 0; word < sentence.length; word++) {
 	    for (var i = 0; i < mshllh.length; i++) {
         if (sentence[word].toLowerCase() === mshllh[i]) {
           message.channel.send({ file: this.constants.mashallah_image }).catch(Logger.exception);
