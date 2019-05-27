ffec0944abf1df1bc5d0eadb3ae86a46d5ec8914
Sylvain Chaugny <sylvain@slaask.com>
2019-05-27 15:09:25 +0200

    [ADD] RandomRename

--- Utils.js
+++ Utils.js
@@ -2,6 +2,22 @@ class Utils {
 	static rand(to = 100) {
 		return Math.floor(Math.random() * (to + 1))
 	}
+
+	static spongeBob(str) {
+		var str = str.toLowerCase().split(' ')
+		var line = ""
+		for (var h = 0; h < str.length; h++) {
+	    for (var i = 0; i < str[h].length; i++) {
+	      if ((i % 2) == 0) {
+	        line += str[h][i].toUpperCase()
+	      } else {
+	        line += str[h][i]
+	      }
+	    }
+	    line += ' '
+		}
+		return line
+	}
 }
 
 module.exports = Utils
