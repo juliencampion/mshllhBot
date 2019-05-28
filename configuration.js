97a07623cb3b790a76133f74def593ffa7c8c00a
Sylvain Chaugny <sylvain@slaask.com>
2019-05-28 17:04:53 +0200

    fix de mort

--- configuration.js
+++ configuration.js
@@ -5,13 +5,13 @@ module.exports = {
 	modules: ["Censorship", "MessageUpdateTracker", "DMMshllhSender", "GayCommand", "MashallahImageSender", "MessageUpdateTracker", "NoHomoMessageSender", "PutTheBell", "RandomInteractions", "9GagReplacer", "AsciiReactions", "RTSiTTrist", "Sudoku", "RandomRename", "DidYouCallMe"],
 	events_configuration: {
 		'message': function(message) {
-			return !message.author.bot && message.author.id != constants.bot_id
+			return !message.author.bot && message.author.id != constants.bot_id && message.content[0] != constants.command_prefix
 		},
 		'messageDelete': function(message) {
-			return true
+			return message.content[0] != constants.command_prefix
 		},
 		'messageUpdate': function(old_m, new_m) {
-			return true
+			return old_m.content[0] != constants.command_prefix
 		}
 	}
 }
